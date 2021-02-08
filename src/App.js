a36074539b253afd7b06996c95e561291ebba0a8
sany168 <saifulxsany68@gmail.com>
2021-02-09 04:44:56 +0600

    lesson 7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-const App = () =>(
-  <div>
-<p>React Appp</p>
-
-</div>
-
-);
+import { Switch, Route} from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+const App = () =>{
+  return(
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 
 
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+
+
+const Home = () =>(
+    <div>
+  <p>React Apsssssspp</p>
+  
+  </div>
+  
+  );
+  
+  
+  
+  export default Home;
+  
