e80b2b30be99bdb3a67bb81de6f04f2813d10bc3
sany168 <saifulxsany68@gmail.com>
2021-02-19 04:27:07 +0600

    lesson 13 email setup

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
-  HomeTwoTone,
+  AppstoreOutlined,
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
-
 import { Link } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
@@ -20,20 +19,17 @@ const Header = () => {
   };
 
   return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" >
-      <Item key="home" icon={<HomeTwoTone />} className="float-left">
-      
-      <Link to="/">Home</Link>
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
       </Item>
 
       <Item key="register" icon={<UserAddOutlined />} className="float-right">
-      
-      <Link to="/register">Register</Link>
+        <Link to="/register">Register</Link>
       </Item>
 
       <Item key="login" icon={<UserOutlined />} className="float-right">
-      
-      <Link to="/login">Login</Link>
+        <Link to="/login">Login</Link>
       </Item>
 
       <SubMenu icon={<SettingOutlined />} title="Username">
